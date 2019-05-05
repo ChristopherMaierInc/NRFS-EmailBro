03d569c293bbe7d6c80e52bb066f2dd04fa3ec2f
Christopher Maier <mail@christophermaier.com.au>
2019-05-05 15:23:26 +1000

    yo

--- index.js
+++ index.js
@@ -16,5 +16,12 @@ passport.use(
 		}
 	)
 );
-    const PORT = process.env.PORT || 5000;
+
+app.get('/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
