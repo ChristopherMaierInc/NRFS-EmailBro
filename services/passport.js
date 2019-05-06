bca87bd71ad144b1f29f15ec4e78b51081455dc8
Christopher Maier <mail@christophermaier.com.au>
2019-05-06 16:17:39 +1000

    serialize & deserializeUser

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-passport.use(
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
+passport.use( 
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
@@ -13,11 +23,14 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
 					if (existingUser) {
-
+						done(null, existingUser);
 					} else {
-						new User({ googleId: profile.id }).save();
+						new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
 					}
 			}) 
 		}
