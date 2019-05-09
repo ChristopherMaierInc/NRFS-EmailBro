520a8ff98431eab36aeecee441cfa241979ccf55
Christopher Maier <mail@christophermaier.com.au>
2019-05-09 16:53:33 +1000

    Fixed Proxy Bug

--- services/passport.js
+++ services/passport.js
@@ -20,19 +20,19 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: keys.googleRedirectURI,
+			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id })
 				.then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-					.save()
-					.then(user => done(null, user));
-				}
+					if (existingUser) {
+						done(null, existingUser);
+					} else {
+						new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+					}
 			}) 
 		}
 	)
